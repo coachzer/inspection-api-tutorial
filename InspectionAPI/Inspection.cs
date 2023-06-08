669d3b55c4d3702d93459657d8fe5e64c105fc6c
coachzer <kovacevicnikola@protonmail.com>
2023-06-08 10:43:12 +0200

    Add project files.

---
+++ InspectionAPI/Inspection.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class Inspection
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string Comments { get; set; } = string.Empty;
+
+        public int InspectionTypeId { get; set; }
+
+        // question mark because it is nullable
+        public InspectionType? InspectionType { get; set; } 
+    }
+}
